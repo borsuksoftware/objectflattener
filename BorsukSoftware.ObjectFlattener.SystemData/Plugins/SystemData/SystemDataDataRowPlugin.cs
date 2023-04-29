8038315e0fb4a2d0d6f4a46cc7f95923580542eb
Steven <steven@borsuksoftware.co.uk>
2023-04-29 11:35:51 +0100

    Add a flattener plugin for DataRow objects

---
+++ BorsukSoftware.ObjectFlattener.SystemData/Plugins/SystemData/SystemDataDataRowPlugin.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorsukSoftware.ObjectFlattener.Plugins.SystemData
+{
+    /// <summary>
+    /// Standard plugin for handling <see cref="System.Data.DataRow"/> objects
+    /// </summary>
+    /// <remarks>In general, the output will be of the format [col]. The exact format depends on the configuration.</remarks>
+    public class SystemDataDataRowPlugin : IObjectFlatteningPlugin
+    {
+        #region Data Model
+
+        /// <summary>
+        /// Get / set how the column portion of the address should be generated
+        /// </summary>
+        public ColumnNamingMode ColumnNamingMode { get; set; } = ColumnNamingMode.ColumnName;
+
+        /// <summary>
+        /// Get / set the custom func to use when <see cref="ColumnNamingMode"/> is equal to <see cref="SystemData.ColumnNamingMode.Custom"/>
+        /// </summary>
+        public Func<int, System.Data.DataColumn, string> ColumnNamingCustomFunc { get; set; }
+
+        /// <summary>
+        /// Get / set the behaviour when an instance of <see cref="DBNull"/> is found
+        /// </summary>
+        public DBNullHandlingMode DBNullHandlingMode { get; set; } = DBNullHandlingMode.Skip;
+
+        #endregion
+
+        public bool CanHandle(string prefix, object @object)
+        {
+            return @object is System.Data.DataRow;
+        }
+
+        public IEnumerable<KeyValuePair<string, object>> FlattenObject(IObjectFlattener objectFlattener, string prefix, object @object)
+        {
+            var row = @object as System.Data.DataRow;
+            if (row == null)
+                throw new InvalidOperationException($"Cannot flatten non DataRow objects - {@object}");
+
+            Func<int, System.Data.DataColumn, string> colNamingFunc;
+            switch (this.ColumnNamingMode)
+            {
+                case ColumnNamingMode.ColumnName:
+                    colNamingFunc = (index, column) =>
+                    {
+                        var columnName = column.ColumnName;
+                        bool quotesRequired = columnName.Contains("]") || columnName.Contains("[");
+                        if (quotesRequired)
+                            return $"[\"{columnName}\"]";
+                        else
+                            return $"[{columnName}]";
+                    };
+                    break;
+
+                case ColumnNamingMode.Index:
+                    colNamingFunc = (index, _) => $"[{index}]";
+                    break;
+
+                case ColumnNamingMode.Custom:
+                    if (this.ColumnNamingCustomFunc == null)
+                        throw new InvalidOperationException($"Custom col naming mode specified, but no custom func was specified");
+
+                    colNamingFunc = this.ColumnNamingCustomFunc;
+                    break;
+
+                default:
+                    throw new InvalidOperationException($"Unsupported col naming mode - {this.ColumnNamingMode}");
+            }
+
+            var columnNames = new List<string>(row.Table.Columns.Count);
+            for (int i = 0; i < row.Table.Columns.Count; i++)
+                columnNames.Add(colNamingFunc(i, row.Table.Columns[i]));
+
+            // Check for duplicates
+            {
+                var columnNamesAsSet = new HashSet<string>();
+                for (int i = 0; i < columnNames.Count; i++)
+                {
+                    if (!columnNamesAsSet.Add(columnNames[i]))
+                        throw new InvalidOperationException($"Duplicate column name '{columnNames[i]}' found (2nd index = #{i}");
+                }
+            }
+
+
+            for (int colIdx = 0; colIdx < row.Table.Columns.Count; colIdx++)
+            {
+                var value = row[colIdx];
+                bool returnValue = false;
+                if (value is DBNull)
+                {
+                    switch (this.DBNullHandlingMode)
+                    {
+                        case DBNullHandlingMode.ReturnDBNull:
+                            returnValue = true;
+                            break;
+
+                        case DBNullHandlingMode.ReturnNull:
+                            returnValue = true;
+                            value = null;
+                            break;
+
+                        case DBNullHandlingMode.Skip:
+                            continue;
+
+                        default:
+                            throw new InvalidOperationException($"Found DBNull instance, but invalid handling mode - {this.DBNullHandlingMode}");
+                    }
+                }
+
+                var colPortion = columnNames[colIdx];
+                var newName = $"{prefix}{colPortion}";
+
+                if (returnValue)
+                    yield return new KeyValuePair<string, object>(newName, value);
+                else
+                    foreach (var valuePair in objectFlattener.FlattenObject(newName, value))
+                        yield return valuePair;
+            }
+        }
+    }
+}
---
+++ BorsukSoftware.ObjectFlattener.Tests/Plugins/SystemData/SystemDataDataRowPluginTests.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Xunit;
+using FluentAssertions;
+using System.Data;
+
+namespace BorsukSoftware.ObjectFlattener.Plugins.SystemData
+{
+    /// <summary>
+    /// Tests for <see cref="SystemDataDataRowPlugin"/>
+    /// </summary>
+    public class SystemDataDataRowPluginTests
+    {
+        #region Duplicate column names
+
+        [Fact]
+        public void CheckRowAsNestedObject()
+        {
+            var dt = new System.Data.DataTable();
+            dt.Columns.Add("Col 1", typeof(string));
+            dt.Columns.Add("Col 2", typeof(int));
+            dt.Columns.Add("Col 3", typeof(double));
+
+            dt.Rows.Add(new object[] { "val1", 2, 2.3 });
+
+            var testObject = new
+            {
+                myRow = dt.Rows[0]
+            };
+
+            var flattener = new ObjectFlattener()
+            {
+                NoAvailablePluginBehaviour = NoAvailablePluginBehaviour.Throw,
+            };
+
+            flattener.Plugins.Add(new SystemDataDataRowPlugin()
+            {
+                ColumnNamingMode = ColumnNamingMode.Index,
+            });
+            flattener.Plugins.Add(new StandardPlugin());
+
+            string prefix = null;
+            var flattenedRepresentation = flattener.FlattenObject(prefix, testObject).ToList();
+
+            flattenedRepresentation.Should().BeEquivalentTo(new KeyValuePair<string, object>[]
+            {
+                new KeyValuePair<string, object>( "myRow[0]", "val1"),
+                new KeyValuePair<string, object>( "myRow[1]", 2),
+                new KeyValuePair<string, object>( "myRow[2]", 2.3),
+            });
+        }
+
+        #endregion
+
+
+        #region Duplicate column names
+
+        [Fact]
+        public void DuplicateColumnName_Throws()
+        {
+            var dt = new System.Data.DataTable();
+            dt.Columns.Add("Col 1", typeof(string));
+            dt.Columns.Add("Col 2", typeof(int));
+            dt.Columns.Add("Col 3", typeof(double));
+
+            dt.Rows.Add(new object[] { "val1", 2, 2.3 });
+
+            var flattener = new ObjectFlattener()
+            {
+                NoAvailablePluginBehaviour = NoAvailablePluginBehaviour.Throw,
+            };
+
+            flattener.Plugins.Add(new SystemDataDataRowPlugin()
+            {
+                ColumnNamingMode = ColumnNamingMode.Custom,
+                ColumnNamingCustomFunc = (_, _) => "Clash",
+            });
+            flattener.Plugins.Add(new StandardPlugin());
+
+            var prefix = "row";
+
+            Assert.Throws<InvalidOperationException>(() => flattener.FlattenObject(prefix, dt.Rows[0]).ToList());
+        }
+
+        #endregion
+
+        #region Column Options
+
+        [Fact]
+        public void Standard_Index_Index()
+        {
+            var dt = new System.Data.DataTable();
+            dt.Columns.Add("Col 1", typeof(string));
+            dt.Columns.Add("Col 2", typeof(int));
+            dt.Columns.Add("Col 3", typeof(double));
+
+            dt.Rows.Add(new object[] { "val1", 2, 2.3 });
+
+            var flattener = new ObjectFlattener()
+            {
+                NoAvailablePluginBehaviour = NoAvailablePluginBehaviour.Throw,
+            };
+
+            flattener.Plugins.Add(new SystemDataDataRowPlugin()
+            {
+                ColumnNamingMode = ColumnNamingMode.Index
+            });
+            flattener.Plugins.Add(new StandardPlugin());
+
+            var prefix = "table";
+            var extractedValues = flattener.FlattenObject(prefix, dt.Rows[0]).ToList();
+
+            var expectedValues = new KeyValuePair<string, object>[] {
+                new KeyValuePair<string, object>( $"{prefix}[0]", "val1" ),
+                new KeyValuePair<string, object>( $"{prefix}[1]", 2 ),
+                new KeyValuePair<string, object>( $"{prefix}[2]", 2.3 ),
+            };
+
+            extractedValues.Should().BeEquivalentTo(expectedValues);
+        }
+
+        [Fact]
+        public void Standard_Custom()
+        {
+            var dt = new System.Data.DataTable();
+            dt.Columns.Add("Col 1", typeof(string));
+            dt.Columns.Add("Col 2", typeof(int));
+            dt.Columns.Add("Col 3", typeof(double));
+
+            dt.Rows.Add(new object[] { "val1", 2, 2.3 });
+
+            var flattener = new ObjectFlattener()
+            {
+                NoAvailablePluginBehaviour = NoAvailablePluginBehaviour.Throw,
+            };
+
+            flattener.Plugins.Add(new SystemDataDataRowPlugin()
+            {
+                ColumnNamingMode = ColumnNamingMode.Custom,
+                ColumnNamingCustomFunc = (idx, col) => $"[Bobble-{idx}-{col}]",
+            });
+            flattener.Plugins.Add(new StandardPlugin());
+
+            var prefix = "table";
+            var extractedValues = flattener.FlattenObject(prefix, dt.Rows[0]).ToList();
+
+            var expectedValues = new KeyValuePair<string, object>[] {
+                new KeyValuePair<string, object>( $"{prefix}[Bobble-0-Col 1]", "val1" ),
+                new KeyValuePair<string, object>( $"{prefix}[Bobble-1-Col 2]", 2 ),
+                new KeyValuePair<string, object>( $"{prefix}[Bobble-2-Col 3]", 2.3 ),
+            };
+
+            extractedValues.Should().BeEquivalentTo(expectedValues);
+        }
+
+        [Fact]
+        public void Standard_Name()
+        {
+            var dt = new System.Data.DataTable();
+            dt.Columns.Add("Col 1", typeof(string));
+            dt.Columns.Add("Col 2", typeof(int));
+            dt.Columns.Add("Col 3]", typeof(double));
+
+            dt.Rows.Add(new object[] { "val1", 2, 2.3 });
+
+            var flattener = new ObjectFlattener()
+            {
+                NoAvailablePluginBehaviour = NoAvailablePluginBehaviour.Throw,
+            };
+
+            flattener.Plugins.Add(new SystemDataDataRowPlugin()
+            {
+                ColumnNamingMode = ColumnNamingMode.ColumnName,
+            });
+            flattener.Plugins.Add(new StandardPlugin());
+
+            var prefix = "table";
+            var extractedValues = flattener.FlattenObject(prefix, dt.Rows[0]).ToList();
+
+            var expectedValues = new KeyValuePair<string, object>[] {
+                new KeyValuePair<string, object>( $"{prefix}[Col 1]", "val1" ),
+                new KeyValuePair<string, object>( $"{prefix}[Col 2]", 2 ),
+                new KeyValuePair<string, object>( $"{prefix}[\"Col 3]\"]", 2.3 ),
+            };
+
+            extractedValues.Should().BeEquivalentTo(expectedValues);
+        }
+        #endregion
+
+        #region DBNull Checks
+
+        [Fact]
+        public void DBNullChecks_Skip()
+        {
+            var dt = new System.Data.DataTable();
+            dt.Columns.Add("Col 1", typeof(string));
+
+            dt.Rows.Add(new object[] { DBNull.Value });
+
+            var flattener = new ObjectFlattener()
+            {
+                NoAvailablePluginBehaviour = NoAvailablePluginBehaviour.Throw,
+            };
+
+            flattener.Plugins.Add(new SystemDataDataRowPlugin()
+            {
+                ColumnNamingMode = ColumnNamingMode.Index,
+                DBNullHandlingMode = DBNullHandlingMode.Skip
+            });
+            flattener.Plugins.Add(new StandardPlugin());
+
+            var prefix = "table";
+            var extractedValues = flattener.FlattenObject(prefix, dt.Rows[0]).ToList();
+            Assert.Empty(extractedValues);
+        }
+
+        [Fact]
+        public void DBNullChecks_Null()
+        {
+            var dt = new System.Data.DataTable();
+            dt.Columns.Add("Col 1", typeof(string));
+            dt.Rows.Add(new object[] { DBNull.Value });
+
+            var flattener = new ObjectFlattener()
+            {
+                NoAvailablePluginBehaviour = NoAvailablePluginBehaviour.Throw,
+            };
+
+            flattener.Plugins.Add(new SystemDataDataRowPlugin()
+            {
+                ColumnNamingMode = ColumnNamingMode.Index,
+                DBNullHandlingMode = DBNullHandlingMode.ReturnNull
+            });
+            flattener.Plugins.Add(new StandardPlugin());
+
+            var prefix = "table";
+            var extractedValues = flattener.FlattenObject(prefix, dt.Rows[0]).ToList();
+
+            var expectedValues = new KeyValuePair<string, object>[] {
+                new KeyValuePair<string, object>( $"{prefix}[0]", null ),
+            };
+
+            extractedValues.Should().BeEquivalentTo(expectedValues);
+        }
+
+        [Fact]
+        public void DBNullChecks_DBNull()
+        {
+            var dt = new System.Data.DataTable();
+            dt.Columns.Add("Col 1", typeof(string));
+            dt.Rows.Add(new object[] { DBNull.Value });
+
+            var flattener = new ObjectFlattener()
+            {
+                NoAvailablePluginBehaviour = NoAvailablePluginBehaviour.Throw,
+            };
+
+            flattener.Plugins.Add(new SystemDataDataRowPlugin()
+            {
+                ColumnNamingMode = ColumnNamingMode.Index,
+                DBNullHandlingMode = DBNullHandlingMode.ReturnDBNull
+            });
+            flattener.Plugins.Add(new StandardPlugin());
+
+            var prefix = "row";
+            var extractedValues = flattener.FlattenObject(prefix, dt.Rows[0]).ToList();
+
+            var expectedValues = new KeyValuePair<string, object>[] {
+                new KeyValuePair<string, object>( $"{prefix}[0]", DBNull.Value ),
+            };
+
+            extractedValues.Should().BeEquivalentTo(expectedValues);
+        }
+
+        #endregion
+    }
+}
